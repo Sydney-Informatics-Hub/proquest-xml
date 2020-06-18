8a04df3bc9364c80373a735576effbbbd2cc67e6
Marius <marius.mather@sydney.edu.au>
2020-06-18 14:19:46 +1000

    Create package skeleton from cookiecutter

---
+++ AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Marius Mather <marius.mather@sydney.edu.au>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ tests/test_proquest_xml.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""Tests for `proquest_xml` package."""
+
+import pytest
+
+
+from proquest_xml import proquest_xml
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
